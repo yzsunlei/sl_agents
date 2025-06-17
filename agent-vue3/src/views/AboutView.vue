576a5ed24dc559ff91f594939388c3fba4d63897
yzsunlei <yzsunlei@qq.com>
2025-06-17 13:07:06 +0800

    feat: update agent-vue3 links

--- agent-vue3/src/views/AboutView.vue
+++ agent-vue3/src/views/AboutView.vue
@@ -3,7 +3,7 @@
     <p>
     一个集成字节-扣子、百度-文心、清华-智谱等多家智能体平台的Agent工具，可以调用多平台的Agent API，并支持解析Markdown、表格、图表、PPT等多种格式内容。
     </p>
-    <ElButton type="primary">跳转文档，了解更多</ElButton>
+    <a href="/docs" target="_blank">跳转文档，了解更多</a>
   </div>
 </template>
 
